6eb72c50661ddaf95d051622dab630ae6492b085
Bradley Smith <brad.smith@sendgrid.com>
2013-07-10 15:03:27 -0600

    broke out cookbook directory to own method

--- lib/berks_effit/file_utils.rb
+++ lib/berks_effit/file_utils.rb
@@ -1,15 +1,25 @@
 require "etc"
+require "fileutils"
 
 module BerksEffit
   class FileUtils
     class << self
       def nuke_cookbook_directory
+        if cookbook_directory_exist?
+          FileUtils.rm_rf(cookbook_directory)
+        else
+          puts "You have already effit!"
+        end
+      end
 
+      def cookbook_directory
+        File.join(default_home, ".berkshelf", "cookbooks")
       end
 
       def cookbook_directory_exist?
-        File.directory?(File.join(default_home, ".berkshelf", "cookbooks"))
+        File.directory?(cookbook_directory)
       end
+
 
       def user_logged_in
         Etc.getlogin  
