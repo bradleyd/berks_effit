752772b84e0823fb880f06b041ce5297a00f7fa2
Bradley Smith <brad.smith@sendgrid.com>
2014-03-28 18:16:34 -0600

    refactored tests

--- test/file_utils_test.rb
+++ test/file_utils_test.rb
@@ -3,14 +3,17 @@ require_relative "test_helper"
 class TestFileUtils < Minitest::Test
   def setup
     @effit  = BerksEffit::FileUtils
+    @effit.stubs(:cookbook_directory).returns("./test/support/cookbooks")
+    BerksEffit::TestHelpers.setup
   end
 
   def test_should_find_berks_directory
-    assert @effit.cookbook_directory_exist?
+    actual = File.directory?("~/.test/support/cookbooks")
+    assert_equal actual, @effit.cookbook_directory_exist?
   end
 
   def test_nuke_cookbook_directoy
-    @effit.stubs(:cookbook_directory).returns("/tmp/foo")
-    assert_equal  "You have already effit!", @effit.nuke_cookbook_directory
+    @effit.nuke_cookbook_directory
+    refute File.directory?("~/.test/support/cookbooks")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,12 @@ require "minitest/pride"
 require "mocha/setup"
 
 require File.expand_path '../../lib/berks_effit.rb', __FILE__
+
+
+module BerksEffit
+  module TestHelpers
+    def self.setup
+      ::FileUtils.mkdir(File.join("./test", "support", "cookbooks"), :noop => true)
+    end
+  end
+end
